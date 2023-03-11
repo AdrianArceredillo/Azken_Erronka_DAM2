13432f665fbd2a82dca98fee5eea248074d37f6d
AdrianArceredillo <arceredillo.adrian@uni.eus>
2023-03-11 13:41:31 +0100

    .

---
+++ VS Proiektua/AzkenErronka_DAM2/AzkenErronka_DAM2/Artistak/frmArtistak.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace AzkenErronka_DAM2.Artistak
+{
+    partial class frmArtistak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmArtistak));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel0_SALIR = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAtzera = new System.Windows.Forms.ToolStripButton();
+            this.p1_Filtros = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel_SUPERIOR = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel0_SALIR.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.p1_Filtros.SuspendLayout();
+            this.panel_SUPERIOR.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(423, 537);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ARTISTAK";
+            // 
+            // panel0_SALIR
+            // 
+            this.panel0_SALIR.Controls.Add(this.toolStrip1);
+            this.panel0_SALIR.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel0_SALIR.Location = new System.Drawing.Point(0, 0);
+            this.panel0_SALIR.Name = "panel0_SALIR";
+            this.panel0_SALIR.Size = new System.Drawing.Size(1294, 25);
+            this.panel0_SALIR.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.Firebrick;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAtzera});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1294, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAtzera
+            // 
+            this.btnAtzera.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAtzera.Image = ((System.Drawing.Image)(resources.GetObject("btnAtzera.Image")));
+            this.btnAtzera.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAtzera.Name = "btnAtzera";
+            this.btnAtzera.Size = new System.Drawing.Size(23, 22);
+            this.btnAtzera.Text = "Atzera";
+            // 
+            // p1_Filtros
+            // 
+            this.p1_Filtros.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p1_Filtros.Controls.Add(this.label6);
+            this.p1_Filtros.Controls.Add(this.textBox4);
+            this.p1_Filtros.Controls.Add(this.textBox3);
+            this.p1_Filtros.Controls.Add(this.label5);
+            this.p1_Filtros.Controls.Add(this.textBox2);
+            this.p1_Filtros.Controls.Add(this.textBox1);
+            this.p1_Filtros.Controls.Add(this.label4);
+            this.p1_Filtros.Controls.Add(this.label3);
+            this.p1_Filtros.Controls.Add(this.label2);
+            this.p1_Filtros.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p1_Filtros.Location = new System.Drawing.Point(0, 25);
+            this.p1_Filtros.Name = "p1_Filtros";
+            this.p1_Filtros.Size = new System.Drawing.Size(1294, 146);
+            this.p1_Filtros.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Artista kodea:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mota:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Deskribapena:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(47, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Izena:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(226, 18);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(31, 83);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(295, 51);
+            this.textBox4.TabIndex = 7;
+            // 
+            // panel_SUPERIOR
+            // 
+            this.panel_SUPERIOR.Controls.Add(this.p1_Filtros);
+            this.panel_SUPERIOR.Controls.Add(this.panel0_SALIR);
+            this.panel_SUPERIOR.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_SUPERIOR.Location = new System.Drawing.Point(0, 0);
+            this.panel_SUPERIOR.Name = "panel_SUPERIOR";
+            this.panel_SUPERIOR.Size = new System.Drawing.Size(1294, 186);
+            this.panel_SUPERIOR.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(382, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Kideak:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 186);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1294, 324);
+            this.panel1.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(1294, 324);
+            this.splitContainer1.SplitterDistance = 431;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // frmArtistak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1294, 606);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel_SUPERIOR);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmArtistak";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Artistak - Musika Bilduma";
+            this.panel0_SALIR.ResumeLayout(false);
+            this.panel0_SALIR.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.p1_Filtros.ResumeLayout(false);
+            this.p1_Filtros.PerformLayout();
+            this.panel_SUPERIOR.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel0_SALIR;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAtzera;
+        private System.Windows.Forms.Panel p1_Filtros;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel_SUPERIOR;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
---
+++ VS Proiektua/AzkenErronka_DAM2/AzkenErronka_DAM2/frmNagusia.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+using System.Data.SqlClient;
+using AzkenErronka_DAM2.Artistak;
+
+namespace AzkenErronka_DAM2
+{
+    public partial class frmNagusia : Form
+    {
+        public frmNagusia()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNagusia_Load(object sender, EventArgs e)
+        {
+            //< add name = "NameofConnectionString" connectionString = "server=YourServerInstanceName; database=DatabaseName; integrated security=SSPI" providerName = "System.Data.SqlClient" />
+
+            //SqlConnection con = new SqlConnection("Data source = LENOVO_ADRI/SQLEXPRESS; Initial catalog = MusikaBilduma; User id = 'sa'; password = 'admin'");
+
+            //con.Open();
+
+
+            //https://www.youtube.com/watch?v=0M0C_d5dxSQ
+            //SqlConnection con = new SqlConnection("Data Source=LENOVO_ADRI\\SQLEXPRESS;Initial Catalog=MusikaBilduma;Integrated Security=True");
+
+
+            //Console.WriteLine("Hola! ");
+        }
+
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            //https://www.youtube.com/watch?v=0M0C_d5dxSQ
+            SqlConnection con = new SqlConnection("Data Source=LENOVO_ADRI\\SQLEXPRESS;Initial Catalog=MusikaBilduma;Integrated Security=True");
+
+            con.Open();
+            MessageBox.Show("sql is connected");
+
+            SqlCommand cmd = new SqlCommand("select * from Kideak", con);//sentencia SQL
+
+            // if the command has no return value
+            cmd.ExecuteNonQuery();
+            //else you might want a Sql data reader to read the return value
+            SqlDataReader read = cmd.ExecuteReader();
+            while (read.Read())
+            {
+                ReadSingleRow((IDataRecord)read);//irakurri lerro bakar bat
+
+                ////do something
+                //Console.WriteLine(read.ToString());
+            }
+
+            //read.Close();// gehiago irakurtzeko ez dagoenean, itxi metodoari deitu
+            //con.Close();//konexioa amaitu edo itxi
+
+
+            frmArtistak frmArtistak = new frmArtistak();
+            frmArtistak.Show();
+
+        }
+
+
+
+        private static void ReadSingleRow(IDataRecord dataRecord)
+        {
+            Console.WriteLine(String.Format("{0}, {1}, {2}", dataRecord[0], dataRecord[1], dataRecord[2]));
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+//BIBLIOGRAFÍA
+
+//Conectarse a SQL Express: https://www.youtube.com/watch?v=0M0C_d5dxSQ
+//REPORTS: https://stackoverflow.com/questions/9700840/how-to-print-a-report-from-an-sql-database-using-visual-basic
+//Save data in DataGridView: https://www.c-sharpcorner.com/article/display-data-in-a-datagridview-C-Sharp-6/
