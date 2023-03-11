b51a284a61e0eade1031052c823316dbd1c250e7
AdrianArceredillo <arceredillo.adrian@uni.eus>
2023-03-11 14:09:16 +0100

    .

--- VS Proiektua/AzkenErronka_DAM2/AzkenErronka_DAM2/Artistak/frmArtistak.Designer.cs
+++ VS Proiektua/AzkenErronka_DAM2/AzkenErronka_DAM2/Artistak/frmArtistak.Designer.cs
@@ -34,35 +34,35 @@
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.btnAtzera = new System.Windows.Forms.ToolStripButton();
             this.p1_Filtros = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.panel_SUPERIOR = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel_SUPERIOR = new System.Windows.Forms.Panel();
+            this.panel_Izquierdo = new System.Windows.Forms.Panel();
+            this.panel_GENERAL = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel_Inferior = new System.Windows.Forms.Panel();
             this.panel0_SALIR.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.p1_Filtros.SuspendLayout();
             this.panel_SUPERIOR.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.SuspendLayout();
+            this.panel_GENERAL.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(423, 537);
+            this.label1.Location = new System.Drawing.Point(516, 83);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.Size = new System.Drawing.Size(193, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "ARTISTAK";
+            this.label1.Text = "ARTISTAK Microsoft Sans Serif; 8,25pt";
             // 
             // panel0_SALIR
             // 
@@ -96,7 +96,7 @@
             // 
             // p1_Filtros
             // 
-            this.p1_Filtros.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p1_Filtros.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.p1_Filtros.Controls.Add(this.label6);
             this.p1_Filtros.Controls.Add(this.textBox4);
             this.p1_Filtros.Controls.Add(this.textBox3);
@@ -106,86 +106,11 @@
             this.p1_Filtros.Controls.Add(this.label4);
             this.p1_Filtros.Controls.Add(this.label3);
             this.p1_Filtros.Controls.Add(this.label2);
-            this.p1_Filtros.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p1_Filtros.Dock = System.Windows.Forms.DockStyle.Fill;
             this.p1_Filtros.Location = new System.Drawing.Point(0, 25);
             this.p1_Filtros.Name = "p1_Filtros";
-            this.p1_Filtros.Size = new System.Drawing.Size(1294, 146);
+            this.p1_Filtros.Size = new System.Drawing.Size(1294, 155);
             this.p1_Filtros.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Artista kodea:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(182, 21);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Mota:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(28, 67);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Deskribapena:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(118, 18);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(47, 20);
-            this.textBox1.TabIndex = 3;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(118, 40);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(208, 20);
-            this.textBox2.TabIndex = 4;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(28, 43);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(36, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Izena:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(226, 18);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 6;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(31, 83);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(295, 51);
-            this.textBox4.TabIndex = 7;
-            // 
-            // panel_SUPERIOR
-            // 
-            this.panel_SUPERIOR.Controls.Add(this.p1_Filtros);
-            this.panel_SUPERIOR.Controls.Add(this.panel0_SALIR);
-            this.panel_SUPERIOR.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel_SUPERIOR.Location = new System.Drawing.Point(0, 0);
-            this.panel_SUPERIOR.Name = "panel_SUPERIOR";
-            this.panel_SUPERIOR.Size = new System.Drawing.Size(1294, 186);
-            this.panel_SUPERIOR.TabIndex = 1;
             // 
             // label6
             // 
@@ -196,33 +121,135 @@
             this.label6.TabIndex = 8;
             this.label6.Text = "Kideak:";
             // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(31, 83);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(295, 51);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(226, 18);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Izena:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(47, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Deskribapena:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mota:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Artista kodea:";
+            // 
+            // panel_SUPERIOR
+            // 
+            this.panel_SUPERIOR.Controls.Add(this.p1_Filtros);
+            this.panel_SUPERIOR.Controls.Add(this.panel0_SALIR);
+            this.panel_SUPERIOR.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_SUPERIOR.Location = new System.Drawing.Point(0, 0);
+            this.panel_SUPERIOR.Name = "panel_SUPERIOR";
+            this.panel_SUPERIOR.Size = new System.Drawing.Size(1294, 180);
+            this.panel_SUPERIOR.TabIndex = 1;
+            // 
+            // panel_Izquierdo
+            // 
+            this.panel_Izquierdo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Izquierdo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_Izquierdo.Location = new System.Drawing.Point(0, 0);
+            this.panel_Izquierdo.Name = "panel_Izquierdo";
+            this.panel_Izquierdo.Size = new System.Drawing.Size(847, 332);
+            this.panel_Izquierdo.TabIndex = 1;
+            // 
+            // panel_GENERAL
+            // 
+            this.panel_GENERAL.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel_GENERAL.Controls.Add(this.panel1);
+            this.panel_GENERAL.Controls.Add(this.panel_Izquierdo);
+            this.panel_GENERAL.Controls.Add(this.panel_Inferior);
+            this.panel_GENERAL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_GENERAL.Location = new System.Drawing.Point(0, 180);
+            this.panel_GENERAL.Name = "panel_GENERAL";
+            this.panel_GENERAL.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel_GENERAL.Size = new System.Drawing.Size(1294, 426);
+            this.panel_GENERAL.TabIndex = 2;
+            // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 186);
+            this.panel1.Location = new System.Drawing.Point(847, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1294, 324);
+            this.panel1.Size = new System.Drawing.Size(447, 332);
             this.panel1.TabIndex = 2;
             // 
-            // splitContainer1
+            // panel_Inferior
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Size = new System.Drawing.Size(1294, 324);
-            this.splitContainer1.SplitterDistance = 431;
-            this.splitContainer1.TabIndex = 0;
+            this.panel_Inferior.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_Inferior.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel_Inferior.Location = new System.Drawing.Point(0, 332);
+            this.panel_Inferior.MaximumSize = new System.Drawing.Size(1294, 94);
+            this.panel_Inferior.MinimumSize = new System.Drawing.Size(1294, 94);
+            this.panel_Inferior.Name = "panel_Inferior";
+            this.panel_Inferior.Size = new System.Drawing.Size(1294, 94);
+            this.panel_Inferior.TabIndex = 0;
             // 
             // frmArtistak
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(1294, 606);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel_GENERAL);
             this.Controls.Add(this.panel_SUPERIOR);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmArtistak";
@@ -235,9 +262,7 @@
             this.p1_Filtros.ResumeLayout(false);
             this.p1_Filtros.PerformLayout();
             this.panel_SUPERIOR.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.panel_GENERAL.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -260,7 +285,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel_SUPERIOR;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel_Izquierdo;
+        private System.Windows.Forms.Panel panel_GENERAL;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel_Inferior;
     }
 }
